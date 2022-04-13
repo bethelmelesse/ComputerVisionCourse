a02e4a37614cf831fb4fa078a0042f62653985c0
Tessema Bethel Melesse <28558632+bettymelesse@users.noreply.github.com>
2022-04-13 15:21:48 +0900

    Linear classifier - implement predict

--- linear_classifier.py
+++ linear_classifier.py
@@ -6,9 +6,11 @@ train_images, train_labels, test_images, test_labels = cifar10(path=None)
 def predict(W, b, X):
     # TODO: Fill out this function, make it return the correct Y = XW+b
     # X is of shape N*D1 array, W is D1*D2 array, b is D2 array.
+    dot_prod = np.dot(X, W)
+    Y = dot_prod + b
+    max_index = np.argmax(Y, axis=1)
 
-    # return Y
-    return None
+    return max_index
 
 
 def l1_loss(W, b, X, Y):
@@ -25,11 +27,14 @@ def svm_loss(W, b, X, Y):
     # return Y
     return None
 
-np.random.seed(12345)
-W = np.random.rand(train_images.shape[0], test_images.shape[1]).astype(train_images.dtype)
-b = np.random.rand(test_images.shape[1]).astype(train_images.dtype)
-ret_y = predict(W, b, train_images)
-print('linear predictions -', ret_y)
+np.random.seed(123456)
+W = np.random.rand(train_images.shape[1], train_labels.shape[1]).astype(train_images.dtype)
+b = np.random.rand(test_labels.shape[1]).astype(train_images.dtype)
+
+ret_y = predict(W, b, test_images)
+print('linear predictions -')
+print(ret_y)
+
 
 # ret_y = l1_loss(W, b, train_images, test_images)
 # print('linear predictions -', ret_y)
